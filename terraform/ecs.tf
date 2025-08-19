02e41e3be84f310e1fe89e7857baebcd32adc3b1
nmnthg <77082885+nmnthg@users.noreply.github.com>
2025-08-19 18:27:36 -0400

    Final Pipeline

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -11,8 +11,8 @@ resource "aws_ecs_task_definition" "app" {
   family                   = "${var.Name_prefix}-taskdef"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = "254"
-  memory                   = "512"
+  cpu                      = 256
+  memory                   = 512
   execution_role_arn       = aws_iam_role.task_execution_role.arn
 
   container_definitions = jsonencode([{
