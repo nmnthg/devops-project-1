38cab5d8fe6ca4fa618c61b913a9a83d08ca70e6
nmnthg <77082885+nmnthg@users.noreply.github.com>
2025-08-19 18:16:46 -0400

    Full pipeline

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,22 +1,22 @@
-# # IAM Roles for ECS task execution
-# data "aws_iam_policy_document" "task_assume" {
-#   statement {
-#     effect = "Allow"
-#     principals {
-#       type        = "Service"
-#       identifiers = ["ecs-tasks.amazonaws.com"]
-#     }
-#     actions = ["sts:AssumeRole"]
-#   }
-# }
+# IAM Roles for ECS task execution
+data "aws_iam_policy_document" "task_assume" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
 
-# resource "aws_iam_role" "task_execution_role" {
-#   name               = "${var.Name_prefix}-task-exec-role"
-#   assume_role_policy = data.aws_iam_policy_document.task_assume.json
-# }
+resource "aws_iam_role" "task_execution_role" {
+  name               = "${var.Name_prefix}-task-exec-role"
+  assume_role_policy = data.aws_iam_policy_document.task_assume.json
+}
 
-# # Attach AWS managed policy for pulling from ECR + CW logs
-# resource "aws_iam_role_policy_attachment" "task_exec_attach" {
-#   role       = aws_iam_role.task_execution_role.name
-#   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-# }
+# Attach AWS managed policy for pulling from ECR + CW logs
+resource "aws_iam_role_policy_attachment" "task_exec_attach" {
+  role       = aws_iam_role.task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
